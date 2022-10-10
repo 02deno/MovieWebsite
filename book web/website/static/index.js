7f5eee9070bd9fa3d4978bc3e67f386643b0973c
02deno <103145955+02deno@users.noreply.github.com>
2022-10-10 23:05:41 +0300

    Delete book web directory

--- book web/website/static/index.js
+++
@@ -1,9 +0,0 @@
-
-function deleteFilm(filmId) {
-    fetch('/delete-film', {
-        method: 'POST',
-        body: JSON.stringify({filmId : filmId}),
-    }).then((_res) => {
-        window.location.href = "/"
-    });
-} 
